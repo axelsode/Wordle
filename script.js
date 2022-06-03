665f54ed493a02df10572d2a9838bdfc321ddd40
axelsode <axel.soderberg1996@gmail.com>
2022-06-03 09:00:53 +0200

    demo

--- script.js
+++ script.js
@@ -11547,11 +11547,9 @@ const DANCE_ANIMATION_DURATION = 500;
 const keyboard = document.querySelector('[data-keyboard]');
 const alertContainer = document.querySelector('[data-alert-container]');
 const guessGrid = document.querySelector('[data-guess-grid]');
-const offsetFromDate = new Date(2022, 0, 1);
-const msOffset = Date.now() - offsetFromDate;
-const dayOffset = msOffset / 1000 / 60 / 60 / 24;
 
-const targetWord = targetWords[Math.floor(msOffset)]
+const targetWord = targetWords[Math.floor(Math.random() * targetWords.length)];
+
 
 startInteraction();
 
