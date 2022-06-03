b739841fb3ab2361dbbe6b750caba23964b9ea30
axelsode <axel.soderberg1996@gmail.com>
2022-06-03 08:58:21 +0200

    1 one/day

--- script.js
+++ script.js
@@ -11547,10 +11547,11 @@ const DANCE_ANIMATION_DURATION = 500;
 const keyboard = document.querySelector('[data-keyboard]');
 const alertContainer = document.querySelector('[data-alert-container]');
 const guessGrid = document.querySelector('[data-guess-grid]');
+const offsetFromDate = new Date(2022, 0, 1);
+const msOffset = Date.now() - offsetFromDate;
+const dayOffset = msOffset / 1000 / 60 / 60 / 24;
 
-
-const targetWord = targetWords[Math.floor(Math.random() * targetWords.length)];
-
+const targetWord = targetWords[Math.floor(msOffset)]
 
 startInteraction();
 
